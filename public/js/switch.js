ce31995cbf005420c0b46c867672e2b193f07922
Alex Scott <runhearn@gmail.com>
2016-06-01 16:11:29 -0500

    Switch upload

---
+++ public/js/switch.js
@@ -0,0 +1,49 @@
+'use strict'
+	var luckyNumber = Math.floor(Math.random()* 6)
+	var cash = 60
+	var Discount1 = "You get a 10% discount!"
+
+	switch (luckyNumber) {
+	case 1:
+		console.log("You have a 10% discount!  That'll be 54 dollars!");
+		break;
+	case 2:
+		console.log("You have a 25% discount!  That'll be 45 dollars!");
+		break;
+	case 3:
+		console.log("No luck today...");
+		break;
+	case 4:
+		console.log("You have a 50% discount!  That'll be 30 dollars!");
+		break;
+	case 5:
+		console.log("Everything is free!");
+		break;
+
+	
+	}
+
+	switch (luckyNumber) {
+	case 1:
+		console.log("It's January!");
+		break;
+	case 2:
+		console.log("It's February!");
+		break;
+	case 3:
+		console.log("It's March!");
+		break;
+	case 4:
+		console.log("It's April!");
+		break;
+	case 5:
+		console.log("It's May!");
+		break;
+
+
+	}
+
+
+
+
+	
---
+++ public/switch.js
@@ -0,0 +1,6 @@
+var luckyNumber = Math.floor(Math.random()* 6)
+	
+	switch (typeof luckyNumber) {
+		case "1":
+		console.log("You are 1");
+	}
