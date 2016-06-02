a764cadd1bd2978cf32a73155a55008221a989e5
Alex Scott <runhearn@gmail.com>
2016-06-02 11:49:04 -0500

    fixed assignement one on do_while js

--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -2,25 +2,24 @@
 
 	var allCones = Math.floor(Math.random() * 50) + 50;
 	var conesremain = allCones;
+	console.log("I have " + allCones + " cones to sell.")
 	do{
 			var cones = Math.floor(Math.random() * 5) + 1;
-			
-		if (cones <= conesremain){
+	if (cones <= conesremain){
 			 conesremain = (conesremain - cones);
 		console.log(cones + " cones sold, " + conesremain + " cones remaining.")
-		} 
-		else console.log(" I cannot sell you " + cones + " I only have " + conesremain)
+	} else console.log(" I cannot sell you " + cones + " I only have " + conesremain)
 		
 	} while (conesremain > 0);
-console.log("Yay! I sold them all!");
+		console.log("Yay! I sold them all!");
 
 //this is assignment 2//
-		var i = 1;
+	var i = 1;
 
-		while (i < 65536) {
+	while (i < 65536) {
 			i = i * 2
 			console.log(i);
-		}
+	}
 
 	
 
