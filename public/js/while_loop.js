4ba66fbfa9111aa4e84a836af10e31db30981055
Alex Scott <runhearn@gmail.com>
2016-06-02 11:40:24 -0500

    fixed js filed with second exercise

--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -13,6 +13,15 @@
 		
 	} while (conesremain > 0);
 console.log("Yay! I sold them all!");
+
+//this is assignment 2//
+		var i = 1;
+
+		while (i < 65536) {
+			i = i * 2
+			console.log(i);
+		}
+
 	
 
 
