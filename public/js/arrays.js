63e8624b127516714b8e781ae6b0a6fe2d34d4fd
Alex Scott <runhearn@gmail.com>
2016-06-03 16:39:25 -0500

    update

--- public/js/arrays.js
+++ public/js/arrays.js
@@ -2,15 +2,15 @@ var names = ['Alex', 'Alan', 'Verona', 'Katie'];
 
 		console.log("There are " + names.length + " names in the names array");
 
-		console.log("For loop:");
+		console.log("Here we go...:");
 
 		
 		for (var i = 0; i < names.length; i++) {
-			console.log("The name being indexed: " + i + " is" + names[i]);
+			console.log("Here's your name: " + i + " is " + names[i]);
 		}
 
-		console.log("ForEach loop");
+		console.log("Here we go.. AGAIN!");
 
 		names.forEach(function (element, index, array) {
-			console.log("The name being indexed: " + index + " is " + names[i]);
+			console.log("Heres your name: " + index + " is " + element);
 		});
