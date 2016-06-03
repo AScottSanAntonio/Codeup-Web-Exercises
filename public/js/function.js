c73a35d8a2a4e86fa7cf9081277a660bc01fc446
Alex Scott <runhearn@gmail.com>
2016-06-03 11:03:39 -0500

    JS function upload

---
+++ public/js/function.js
@@ -0,0 +1,18 @@
+	var myNameIs = 'Alex'; 
+			
+			function sayHello(name) {
+				console.log("Hello, " + name + "!");
+			}
+
+			
+			sayHello(myNameIs);
+
+			
+			var random = Math.floor((Math.random()*100)+1);
+
+			console.log("Random number is: " + random.toString());
+			
+			
+			function isOdd(numberToTest) {
+				(numberToTest % 2 === 1) ? console.log("Number was odd") : console.log("Number was even")
+			}
