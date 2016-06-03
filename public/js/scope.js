fe2ed7959ed32640363249c472da832b7970ca14
Alex Scott <runhearn@gmail.com>
2016-06-03 15:39:09 -0500

    scope js upload

---
+++ public/js/scope.js
@@ -0,0 +1,24 @@
+	(function() {
+
+
+	var myNameIs = 'Alex'; 
+			
+			function sayHello(name) {
+				console.log("Hello, " + name + "!");
+			}
+
+			
+			sayHello(myNameIs);
+
+			
+			var random = Math.floor((Math.random()*100)+1);
+
+			console.log("Random number is: " + random.toString());
+			
+			
+			function isOdd(numberToTest) {
+				(numberToTest % 2 === 1) ? console.log("Number was odd") : console.log("Number was even");
+			}
+
+			isOdd(random);
+	})();
